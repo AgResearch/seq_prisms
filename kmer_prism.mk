f0e00cdc0085dc96641573ac2922077c6832ab90
afmcc <alanfmcculloch@gmail.com>
2018-03-10 13:37:12 +1300

    adding kmer prism

---
+++ kmer_prism.mk
@@ -0,0 +1,41 @@
+# kmer_prism main makefile
+#***************************************************************************************
+# references:
+#***************************************************************************************
+# make: 
+#     http://www.gnu.org/software/make/manual/make.html
+#
+data_dir=
+out_dir=
+hpc_type=
+sample_rate=
+
+ifeq ($(strip $(sample_rate)),)
+sample_args=
+else
+sample_args=-s $(sample_rate)
+endif
+
+# this is for testing - these will be pulled out from a look-up file
+kmer_parameters=-t frequency -k 6 -p 4
+
+##############################################
+# how to make kmer summary. Note that this will pickle the kmer distribution
+# in $(out_dir), so that a subsequent call to kmer_entropy can build the 
+# complete spectra.
+##############################################
+%.kmer_prism:
+	tardis.py -hpctype $(hpc_type) -d $(out_dir) kmer_entropy.py $(sample_args) $(kmer_parameters) -o $(out_dir)/$(notdir $*).frequency.txt -b $(out_dir) $(data_dir)/$(notdir $*)
+	date > $@
+	
+##############################################
+# specify the intermediate files to keep 
+##############################################
+.PRECIOUS: %.log %.kmer_prism
+
+##############################################
+# cleaning - not yet doing this using make  
+##############################################
+clean:
+	echo "no clean for now" 
+
---
+++ kmer_prism.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+function get_opts() {
+
+   PWD0=$PWD
+   DRY_RUN=no
+   DEBUG=no
+   HPC_TYPE=slurm
+   FILES=
+   OUT_DIR=
+   DATA_DIR=
+   SAMPLE_RATE=
+   MAX_TASKS=50
+
+
+   help_text="
+\n
+./kmer_prism.sh  [-h] [-n] [-d] [-s SAMPLE_RATE] -D datadir -O outdir [-C local|slurm ] input_file_names\n
+\n
+\n
+example:\n
+kmer_prism.sh -n -D /dataset/Tash_FL1_Ryegrass/ztmp/For_Alan -O /dataset/Tash_FL1_Ryegrass/ztmp/seq_qc/test/fastqc  /dataset/Tash_FL1_Ryegrass/ztmp/For_Alan/*.fastq.gz\n
+\n
+"
+
+   # defaults:
+   while getopts ":nhO:C:D:s:m:" opt; do
+   case $opt in
+       n)
+         DRY_RUN=yes
+         ;;
+       d)
+         DEBUG=yes
+         ;;
+       h)
+         echo -e $help_text
+         exit 0
+         ;;
+       O)
+         OUT_DIR=$OPTARG
+         ;;
+       D)
+         DATA_DIR=$OPTARG
+         ;;
+       C)
+         HPC_TYPE=$OPTARG
+         ;;
+       s)
+         SAMPLE_RATE=$OPTARG
+         ;;
+       m)
+         MAX_TASKS=$OPTARG
+         ;;
+       \?)
+         echo "Invalid option: -$OPTARG" >&2
+         exit 1
+         ;;
+       :)
+         echo "Option -$OPTARG requires an argument." >&2
+         exit 1
+         ;;
+     esac
+   done
+
+   shift $((OPTIND-1))
+
+   FILES=$@
+}
+
+
+function check_opts() {
+   if [ ! -d $OUT_DIR ]; then
+      echo "OUT_DIR $OUT_DIR not found"
+      exit 1
+   fi
+   if [ ! -d $DATA_DIR ]; then
+      echo "DATA_DIR $DATA_DIR not found"
+      exit 1
+   fi
+
+   if [[ $HPC_TYPE != "local" && $HPC_TYPE != "slurm" ]]; then
+      echo "HPC_TYPE must be one of local, slurm"
+      exit 1
+   fi
+}
+
+function echo_opts() {
+  echo DATA_DIR=$DATA_DIR
+  echo OUT_DIR=$OUT_DIR
+  echo DRY_RUN=$DRY_RUN
+  echo DEBUG=$DEBUG
+  echo HPC_TYPE=$HPC_TYPE
+  echo FILES=$FILES
+  echo SAMPLE_RATE=$SAMPLE_RATE
+}
+
+#
+# edit this method to set required environment (or set up
+# before running this script)
+#
+function configure_env() {
+   cd $SEQ_PRISMS_BIN
+   cp ./kmer_prism.sh $OUT_DIR
+   cp ./kmer_prism.mk $OUT_DIR
+   cp ./kmer_entropy.py $OUT_DIR
+   cp ./kmer_plots.r $OUT_DIR
+   echo "
+[tardish]
+[tardis_engine]
+max_tasks=$MAX_TASKS
+" > $OUT_DIR/.tardishrc
+   cd $OUT_DIR
+}
+
+
+function check_env() {
+   if [ -z "$SEQ_PRISMS_BIN" ]; then
+      echo "SEQ_PRISMS_BIN not set - exiting"
+      exit 1
+   fi
+}
+
+function get_targets() {
+   TARGETS=""
+   for file in $FILES; do
+      base=`basename $file`
+      TARGETS="$TARGETS $OUT_DIR/${base}.kmer_prism"
+   done 
+}
+
+
+function fake_prism() {
+   echo "dry run ! "
+   make -n -f kmer_prism.mk -d -k  --no-builtin-rules -j 16 hpc_type=$HPC_TYPE sample_rate=$SAMPLE_RATE data_dir=$DATA_DIR out_dir=$OUT_DIR  $TARGETS > $OUT_DIR/kmer_prism.log 2>&1
+   echo "dry run : summary commands are 
+   tardis.py -q -hpctype $HPC_TYPE -d $OUT_DIR $OUT_DIR/kmer_entropy.py -s .00015 -t zipfian -k 8 -p 30 -o zipfian8.txt -b /dataset/Tash_FL1_Ryegrass/ztmp/seq_qc/kmer_analysis /dataset/Tash_FL1_Ryegrass/ztmp/For_Alan/*.fastq.gz
+
+   tardis.py -hpctype $HPC_TYPE -d $OUT_DIR Rscript --vanilla  $OUT_DIR/mapping_stats_plots.r datafolder=$OUT_DIR
+   "
+   exit 0
+}
+
+function run_prism() {
+   # this distributes the kmer distribtion builds for each file across the cluster
+   make -f kmer_prism.mk -d -k  --no-builtin-rules -j 16 hpc_type=$HPC_TYPE sample_rate=$SAMPLE_RATE data_dir=$DATA_DIR out_dir=$OUT_DIR $TARGETS > $OUT_DIR/kmer_prism.log 2>&1
+   # this uses the pickled distributions to make the final spectra
+   tardis.py -hpctype $HPC_TYPE -d $OUT_DIR kmer_entropy.py -k 6 -t zipfian -o $OUT_DIR/kmer_summay.txt -b $OUT_DIR $TARGETS
+   # do plots. This will fail until we sort out R env - need to run manually 
+   #/dataset/bioinformatics_dev/active/R3.3/R-3.3.0/bin/Rscript --vanilla  $(GBS_BIN)/kmer_plots_gbs.r datafolder=$(dir $@)
+   tardis.py -hpctype $HPC_TYPE -d $OUT_DIR Rscript --vanilla kmer_plots_gbs.r datafolder=$OUT_DIR
+}
+
+function html_prism() {
+   echo "tba" > $OUT_DIR/kmer_prism.html 2>&1
+}
+
+
+function main() {
+   get_opts $@
+   check_opts
+   echo_opts
+   check_env
+   get_targets
+   configure_env
+   if [ $DRY_RUN != "no" ]; then
+      fake_prism
+   else
+      run_prism
+      if [ $? == 0 ] ; then
+         html_prism
+      else
+         echo "error state from kmer run - skipping html page generation"
+         exit 1
+      fi
+   fi
+}
+
+
+set -x
+main $@
+set +x
+
