ac23c59988dc5f49258667245de2538ebf76c166
afmcc <alanfmcculloch@gmail.com>
2018-03-22 14:10:04 +1300

    align prism updates

--- align_prism.sh
+++ align_prism.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+declare -a references_array
+declare -a parameters_array
+
 function get_opts() {
 
    PWD0=$PWD
@@ -8,7 +12,6 @@ function get_opts() {
    HPC_TYPE=slurm
    FILES=""
    OUT_DIR=""
-   DATA_DIR=""
    SAMPLE_RATE=""
    MAX_TASKS=50
    ALIGNER=blastn
@@ -20,7 +23,7 @@ function get_opts() {
 
    help_text="
 \n
-./bwa_prism.sh  [-h] [-n] [-d] [-f] [-s SAMPLE_RATE] -a aligner -D datadir -O outdir [-C local|slurm ] input_file_names\n
+./align_prism.sh  [-h] [-n] [-d] [-f] [-s SAMPLE_RATE] -a aligner -r [ref name | file of ref names ] -p [ parameters or file of parameters ] -O outdir [-C local|slurm ] input_file_names\n
 \n
 \n
 example:\n
@@ -29,7 +32,7 @@ bwa_prism.sh -n -D /dataset/Tash_FL1_Ryegrass/ztmp/For_Alan -O /dataset/Tash_FL1
 "
 
    # defaults:
-   while getopts ":nhO:C:D:s:m:a:" opt; do
+   while getopts ":nhO:C:s:m:a:r:p:" opt; do
    case $opt in
        n)
          DRY_RUN=yes
@@ -46,9 +49,6 @@ bwa_prism.sh -n -D /dataset/Tash_FL1_Ryegrass/ztmp/For_Alan -O /dataset/Tash_FL1
          ;;
        O)
          OUT_DIR=$OPTARG
-         ;;
-       D)
-         DATA_DIR=$OPTARG
          ;;
        C)
          HPC_TYPE=$OPTARG
@@ -90,17 +90,12 @@ function check_opts() {
       echo "OUT_DIR $OUT_DIR not found"
       exit 1
    fi
-   if [ ! -d $DATA_DIR ]; then
-      echo "DATA_DIR $DATA_DIR not found"
-      exit 1
-   fi
-
    if [[ $HPC_TYPE != "local" && $HPC_TYPE != "slurm" ]]; then
       echo "HPC_TYPE must be one of local, slurm"
       exit 1
    fi
-   if [[ $ALIGNER != "blastn" && $ALIGNER != "bwa" ]]; then
-      echo "ALIGNER must be one of blastn, bwa"
+   if [[ $ALIGNER != "blastn" && $ALIGNER != "qblastn" && $ALIGNER != "bwa" ]]; then
+      echo "ALIGNER must be one of blastn, qblastn, bwa"
       exit 1
    fi
    if [ $REFERENCES == none ]; then
@@ -108,42 +103,59 @@ function check_opts() {
       exit 1
    fi
 
-   # construct array of references (read from file if necessary)
-   declare -a references_array
-   declare -a parameters_array
 
-
+   ##### note this needs re-working so can specify e.g. ref on command line, but a file of parameters ,
+   # and we apply multiple parameters sets
    if [ ! -f $REFERENCES ]; then
       # assume just one reference, specified on command line
       references_array[1]=$REFERENCES
 
-      if [ -z "$PARAMETERS  ]; then
+
+      if [ -z "$PARAMETERS"  ]; then
          echo "warning , no alignment parameters supplied, $ALIGNER defaults will apply"
       else
          parameters_array[1]=$PARAMETERS      
       fi
+      NUM_REFERENCES=1
+      NUM_PARAMETERS=1
+
    else  # we have more than one reference to set up (or one ref, but different parameter sets)
-      if [ ! f $PARAMETERS ]; then
-         echo "argument to -p must be a filename containing alignment parameters, with the same number of records as $REFERENCES"
-         exit 1
+      if [ ! -f $PARAMETERS ]; then
+         if [ -z $PARAMETERS ]; then
+            echo "warning , no alignment parameters supplied, $ALIGNER defaults will apply"
+         fi
       fi
       index=1
+      IFS=$'\n'
       for record in `awk '/\S+/{print}' $REFERENCES`; do
          references_array[$index]=$record
          let index=$index+1
       done
-      index=1
-      for record in `awk '/\S+/{print}' $PARAMETERS`; do
-         parameters_array[$index]=$record
-         let index=$index+1
-      done
-
       NUM_REFERENCES=${#references_array[*]}
+      index=1
+      if [ -f $PARAMETERS ]; then
+         for record in `awk '/\S+/{print}' $PARAMETERS`; do
+            parameters_array[$index]=$record
+            let index=$index+1
+         done
+      else
+         for ((i=1;$i<=$NUM_REFERENCES;i=$i+1)) do
+            parameters_array[$i]=$PARAMETERS      
+         done
+      fi
+
+      unset IFS 
+
+      NUM_PARAMETERS=${#parameters_array[*]}
+
+      if [ $NUM_REFERENCES != $NUM_PARAMETERS ]; then
+         echo "error,  have $NUM_REFERENCES references but $NUM_PARAMETERS parameters"
+         exit 1
+      fi
    fi
 }
 
 function echo_opts() {
-  echo DATA_DIR=$DATA_DIR
   echo OUT_DIR=$OUT_DIR
   echo DRY_RUN=$DRY_RUN
   echo DEBUG=$DEBUG
@@ -151,8 +163,14 @@ function echo_opts() {
   echo FILES=$FILES
   echo SAMPLE_RATE=$SAMPLE_RATE
   echo ALIGNER=$ALIGNER
-  echo ALIGNER=$ALIGNER
-
+  echo REFEREENCES
+  for ((i=1;$i<=$NUM_REFERENCES;i=$i+1)) do
+     echo ${references_array[$i]}
+  done
+  echo PARAMETERS
+  for ((i=1;$i<=$NUM_PARAMETERS;i=$i+1)) do
+     echo ${parameters_array[$i]}
+  done
 }
 
 #
@@ -173,8 +191,8 @@ max_tasks=$MAX_TASKS
    cd $OUT_DIR
    echo "
 source activate /dataset/bioinformatics_dev/active/conda-env/biopython
-PATH="$OUT_DIR:$PATH"
-PYTHONPATH="$OUT_DIR:$PYTHONPATH"
+PATH=\"$OUT_DIR:$PATH\"
+PYTHONPATH=\"$OUT_DIR:$PYTHONPATH\"
 " > $OUT_DIR/configure_env.sh
 }
 
@@ -196,7 +214,7 @@ function get_targets() {
       for file in $FILES; do
          file_base=`basename $file`
 	 ref_base=`basename ${references_array[$i]}`
-         parameters_moniker=`echo ${parameters_array[$i]} | sed 's/ //g' | sed 's/\//\./g`
+         parameters_moniker=`echo ${parameters_array[$i]} | sed 's/ //g' | sed 's/\//\./g'`
          alignment_moniker=${file_base}.${ALIGNER}.${ref_base}.${parameters_moniker}
          TARGETS="$TARGETS $OUT_DIR/${alignment_moniker}.align_prism"
 
@@ -209,30 +227,36 @@ function get_targets() {
          reference=${references_array[$i]}
          parameters=${parameters_array[$i]}
 
-         aligner_filename=$OUT_DIR/${alignment_moniker}.src
+         aligner_filename=$OUT_DIR/${alignment_moniker}.sh
 
          if [ -f aligner_filename ]; then
             if [ ! $FORCE == yes ]; then
-               echo "found existing aligner $aligner_filename - will re-use (use -f to force rebuild of aligners)"
+               echo "found existing aligner $aligner_filename - will re-use (use -f to force rebuild of aligners) "
                continue
             fi
          fi
 
          if [ $ALIGNER == bwa ]; then
-            echo "
-            source /dataset/bioinformatics_dev/scratch/tardis/bin/activate
-            tardis -hpctype $HPC_TYPE -d  $OUT_DIR  $sample_phrase bwa aln $parameters _condition_fastq_input_$file \> _condition_throughput_$(OUT_DIR)/${alignment_moniker}.sai \; bwa samse $reference _condition_throughput_$(OUT_DIR)/${alignment_moniker}.sai _condition_fastq_input_$file  \> _condition_sam_output_$(OUT_DIR)/${alignment_moniker}.bam  
-	    tardis -hpctype $HPC_TYPE -q -d $OUT_DIR samtools $(OUT_DIR)/${alignment_moniker}.bam   > $(OUT_DIR)/${alignment_moniker}.stats 
+            echo "#!/bin/bash
+source /dataset/bioinformatics_dev/scratch/tardis/bin/activate
+tardis -hpctype $HPC_TYPE -d  $OUT_DIR  $sample_phrase bwa aln $parameters _condition_fastq_input_$file \> _condition_throughput_$OUT_DIR/${alignment_moniker}.sai \; bwa samse $reference _condition_throughput_$OUT_DIR/${alignment_moniker}.sai _condition_fastq_input_$file  \> _condition_sam_output_$OUT_DIR/${alignment_moniker}.bam  
+tardis -hpctype $HPC_TYPE -q -d $OUT_DIR samtools $OUT_DIR/${alignment_moniker}.bam   > $OUT_DIR/${alignment_moniker}.stats 
             " > $aligner_filename
          elif [ $ALIGNER == blastn ]; then
-            echo "
-            source /dataset/bioinformatics_dev/scratch/tardis/bin/activate
-            tardis -hpctype $HPC_TYPE -d  $OUT_DIR  $sample_phrase blastn -db $reference -query  _condition_fastq2fasta_input_$file $parameters \> _condition_text_output_$(OUT_DIR)/${alignment_moniker}.results   
+            echo "#!/bin/bash
+source /dataset/bioinformatics_dev/scratch/tardis/bin/activate
+tardis -hpctype $HPC_TYPE -d  $OUT_DIR  $sample_phrase blastn -db $reference -query  _condition_fasta_input_$file $parameters \> _condition_text_output_$OUT_DIR/${alignment_moniker}.results   
+            " > $aligner_filename
+         elif [ $ALIGNER == qblastn ]; then
+            echo "#!/bin/bash
+source /dataset/bioinformatics_dev/scratch/tardis/bin/activate
+tardis -hpctype $HPC_TYPE -d  $OUT_DIR  $sample_phrase blastn -db $reference -query  _condition_fastq2fasta_input_$file $parameters \> _condition_text_output_$OUT_DIR/${alignment_moniker}.results   
             " > $aligner_filename
          else 
-            echo "unsupported aligner $ALIGNER"
+            echo "unsupported aligner $ALIGNER "
             exit 1
          fi
+         chmod +x $aligner_filename
       done
    done 
 }
@@ -242,18 +266,18 @@ function fake_prism() {
    echo "dry run ! "
    make -n -f align_prism.mk -d -k  --no-builtin-rules -j 16 $TARGETS > $OUT_DIR/align_prism.log 2>&1
    echo "dry run : summary commands are 
-   (TBA - probably another make)
+   TBA - probably another make
    "
    exit 0
 }
 
 function run_prism() {
-   make -n -f align_prism.mk -d -k  --no-builtin-rules -j 16 $TARGETS > $OUT_DIR/align_prism.log 2>&1
+   make -f align_prism.mk -d -k  --no-builtin-rules -j 16 $TARGETS > $OUT_DIR/align_prism.log 2>&1
    # will call another make to do summaries
 }
 
 function html_prism() {
-   echo "tba" > $OUT_DIR/bwa_prism.html 2>&1
+   echo "tba" > $OUT_DIR/align_prism.html 2>&1
 }
 
 
@@ -281,4 +305,3 @@ function main() {
 set -x
 main $@
 set +x
-
